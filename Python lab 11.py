cf18ac839848c1ec18d157984cdaa268438251e0
Vaikundhavasan <121146276+Vaikundhavasan@users.noreply.github.com>
2023-01-05 15:40:24 +0530

    Update and rename Python lab 11.py to ADS 2-b).cpp

--- Python lab 11.py
+++
@@ -1,7 +0,0 @@
-color = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
-with open('abc.txt', "w") as myfile:
-        for c in color:
-                myfile.write( c + "\n" )
-
-content = open('abc.txt')
-print(content.read())
